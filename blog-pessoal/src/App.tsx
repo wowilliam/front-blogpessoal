d448078b4f7b17da7e2ab8574597c378f36a1b5b
wowilliam <wo.william@hotmail.com>
2022-03-29 15:10:24 -0300

    Adicinando Link

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <switch>
+        <div style={{ minHeight: "100vh" }}>
+        <Route exact path="/">
+            <Login />
+          </Route>
+
+        <Route path='/login'>
+              <Login />
+          </Route>
+
+          <Route path='/home'>
+              <Home />
+          </Route>
+          
+        </div>
+      </switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
